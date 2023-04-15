eeae2b767ba857a319aa31aac537213e8be100b3
Karthik Namboori <103726023+karthik2603-theBrogrammer@users.noreply.github.com>
2023-04-15 16:51:45 +0530

    Update SocketProvider.jsx

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("https://backend-two-swart.vercel.app/"), []);
+  const socket = useMemo(() => io("localhost:8000"), []);
 
   return (
     <SocketContext.Provider value={socket}>
